a4412ee6830e3271731454f52c1ea5eaca15e07b
Attila Molnar <ate.molnar2@gmail.com>
2018-03-19 14:24:58 +0100

    Feautre: window-size responsive slider

--- slider.js
+++ slider.js
@@ -3,212 +3,249 @@
 // register the application module
 b4w.register("slider_main", function(exports, require) {
 
-	// import modules used by the app
-	var m_app       = require("app");
-	var m_cfg       = require("config");
-	var m_data      = require("data");
-	var m_preloader = require("preloader");
-	var m_ver       = require("version");
-	var m_scenes    = require("scenes");
-	var m_ctl    = require("controls");
-	var m_trans = require("transform");
-	var m_cam = require("camera");
-	var m_math = require("math");
-	var m_phy = require("physics");
-	var m_vec3 = require("vec3");
-	var m_material = require("material");
+  // import modules used by the app
+  var m_app       = require("app");
+  var m_cfg       = require("config");
+  var m_data      = require("data");
+  var m_preloader = require("preloader");
+  var m_ver       = require("version");
+  var m_scenes    = require("scenes");
+  var m_ctl    = require("controls");
+  var m_trans = require("transform");
+  var m_cam = require("camera");
+  var m_math = require("math");
+  var m_phy = require("physics");
+  var m_vec3 = require("vec3");
+  var m_material = require("material");
+  var m_anim = require("animation");
 
-	// detect application mode
-	var DEBUG = (m_ver.type() == "DEBUG");
+  // detect application mode
+  var DEBUG = (m_ver.type() == "DEBUG");
 
-	// automatically detect assets path
-	var APP_ASSETS_PATH = m_cfg.get_assets_path("slider");
+  // automatically detect assets path
+  var APP_ASSETS_PATH = m_cfg.get_assets_path("slider");
 
-	/**
-	 * export the method to initialize the app (called at the bottom of this file)
-	 */
-	exports.init = function() {
-		m_app.init({
-			canvas_container_id: "main_canvas_container",
-			callback: init_cb,
-			show_fps: DEBUG,
-			console_verbose: DEBUG,
-			physics_enabled: true,
-			autoresize: true
-		});
-	}
+  /**
+   * export the method to initialize the app (called at the bottom of this file)
+   */
+  exports.init = function() {
+    m_app.init({
+      canvas_container_id: "main_canvas_container",
+      callback: init_cb,
+      show_fps: DEBUG,
+      console_verbose: DEBUG,
+      physics_enabled: true,
+      autoresize: true
+    });
+  }
 
-	/**
-	 * callback executed when the app is initialized
-	 */
-	function init_cb(canvas_elem, success) {
+  /**
+   * callback executed when the app is initialized
+   */
+  function init_cb(canvas_elem, success) {
 
-		if (!success) {
-			console.log("b4w init failure");
-			return;
-		}
+    if (!success) {
+      console.log("b4w init failure");
+      return;
+    }
 
-		m_preloader.create_preloader();
+    m_preloader.create_preloader();
 
-		// ignore right-click on the canvas element
-		canvas_elem.oncontextmenu = function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-			return false;
-		};
+    // ignore right-click on the canvas element
+    canvas_elem.oncontextmenu = function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    };
 
-		load();
-	}
+    load();
+  }
 
-	/**
-	 * load the scene data
-	 */
-	function load() {
-		m_data.load(APP_ASSETS_PATH + "slider.json", load_cb, preloader_cb);
-	}
+  /**
+   * load the scene data
+   */
+  function load() {
+    m_data.load(APP_ASSETS_PATH + "slider.json", load_cb, preloader_cb);
+  }
 
-	/**
-	 * update the app's preloader
-	 */
-	function preloader_cb(percentage) {
-		m_preloader.update_preloader(percentage);
-	}
+  /**
+   * update the app's preloader
+   */
+  function preloader_cb(percentage) {
+    m_preloader.update_preloader(percentage);
+  }
 
-	/**
-	 * callback executed when the scene data is loaded
-	 */
-	function load_cb(data_id, success) {
+  /**
+   * callback executed when the scene data is loaded
+   */
+  function load_cb(data_id, success) {
 
-		if (!success) {
-			console.log("b4w load failure");
-			return;
-		}
+    if (!success) {
+      console.log("b4w load failure");
+      return;
+    }
 
-		var camObj = m_scenes.get_active_camera();
-		var railObj = m_scenes.get_object_by_name("rail");
-		var knobObj = m_scenes.get_object_by_name("knob");
+    var camObj = m_scenes.get_active_camera();
+    var railObj = m_scenes.get_object_by_name("rail");
+    var knobObj = m_scenes.get_object_by_name("knob");
 
-		m_app.enable_camera_controls();
+    // m_app.enable_camera_controls();
 
-		registerSlider(railObj, knobObj, camObj, 0.3, ["rail", "slider-output2"], ["knob", "slider-output"]);
+    registerSliderResize(railObj, knobObj, 60);
+    registerSlider(railObj, knobObj, camObj, 0.3, ["rail", "slider-output2"], ["knob", "slider-output"]);
 
-	}
+  }
 
-	function b4wObjEqual(obj1, obj2) {
-		if (obj1.uuid === obj2.uuid) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+  function b4wObjEqual(obj1, obj2) {
+    if (obj1.uuid === obj2.uuid) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
+  function registerSliderResize(railObj, knobObj, frameNum) {
 
-	function registerSlider(railObj, knobObj, camObj, defVal, railNodemat, knobNodemat ) {
+    var etalonRatio = 16 / 9;
 
-		var sliderLength = m_trans.get_object_size(railObj) * 2;
+    function resize() {
 
-		// sensor
-		var mouseSensor = m_ctl.create_mouse_move_sensor();
-		var clickSensor = m_ctl.create_mouse_click_sensor();
+      var ratio = window.innerWidth / window.innerHeight;
+      var frameInd = Math.floor(ratio / etalonRatio * frameNum);
 
-		// state
-		var startTrans;
-		var startCanvasX;
-		var startCanvasY;
+      if (frameInd > frameNum)
+        frameInd = frameNum;
+      else if (frameInd < 1)
+        frameInd = 1;
 
-		// optimization state
-		var from = new Float32Array(3);
-		var pline = m_math.create_pline();
-		var to = new Float32Array(3);
+      // invert frameInd
+      frameInd = frameNum - frameInd;
 
-		var grabbedP = false;
+      m_anim.apply(railObj, "rail-resize", m_anim.SLOT_0);
+      m_anim.set_behavior(railObj, m_anim.AB_FINISH_STOP, m_anim.SLOT_0);
+      m_anim.play(railObj, null, m_anim.SLOT_0);
+      console.log(frameInd);
+      m_anim.set_frame(railObj, frameInd, m_anim.SLOT_0);
+      m_anim.stop(railObj, m_anim.SLOT_0);
 
-		// init slider position
-		m_trans.set_translation_obj_rel(knobObj, sliderLength * (defVal - 0.5), 0, 0.1, railObj);
+      m_anim.apply(knobObj, "knob-resize", m_anim.SLOT_0);
+      m_anim.set_behavior(railObj, m_anim.AB_FINISH_STOP, m_anim.SLOT_0);
+      m_anim.play(railObj, null, m_anim.SLOT_0);
+      console.log(frameInd);
+      m_anim.set_frame(railObj, frameInd, m_anim.SLOT_0);
+      m_anim.stop(railObj, m_anim.SLOT_0);
+    };
 
+    resize();
 
-		function slide(x, y) {
+    window.addEventListener("resize", resize);
+  }
 
-			function rayTestCb(id, hitFraction, objHit, hitTime, hitPos, hitNorm) {
-				// objHit is always calculated against the center of mass.
-				var normalizedOutput = hitPos[0] / sliderLength + 0.5;
-				console.log(normalizedOutput);
-				if (normalizedOutput > 0 && normalizedOutput < 1)
-					m_trans.set_translation_obj_rel(knobObj, hitPos[0], 0, 0.1, railObj);
-				if (railNodemat)
-					m_material.set_nodemat_value(railObj, railNodemat, normalizedOutput);
-				if (knobNodemat)
-					m_material.set_nodemat_value(knobObj, knobNodemat, normalizedOutput);
+  function registerSlider(railObj, knobObj, camObj, defVal, railNodemat, knobNodemat ) {
 
-			}
+    var sliderLength = m_trans.get_object_size(railObj) * 2;
 
-			m_cam.calc_ray(camObj, x, y, pline);
-			m_math.get_pline_directional_vec(pline, to);
-			m_vec3.scale(to, 100, to);
-			var id = m_phy.append_ray_test_ext(
-				camObj,
-				from,
-				to,
-				"hud",
-				rayTestCb,
-				true,
-				false,
-				true,
-				true
-			);
+    window.addEventListener("resize", function () {
+      sliderLength = m_trans.get_object_size(railObj) * 2;
+      console.log("len", sliderLength);
+    });
 
-		}
+    // sensor
+    var mouseSensor = m_ctl.create_mouse_move_sensor();
+    var clickSensor = m_ctl.create_mouse_click_sensor();
 
-		function logic(triggers) {
-			if (triggers[1])
-				return 1;
-			else
-				return 0;
-		}
+    // optimization state
+    var from = new Float32Array(3);
+    var pline = m_math.create_pline();
+    var to = new Float32Array(3);
 
-		function cb(obj, id, pulse, param) {
-			var pxPerFrame = m_ctl.get_sensor_value(obj, id, 0);
-			var pos = m_ctl.get_sensor_payload(obj, id, 0);
-			var click = m_ctl.get_sensor_payload(obj, id, 1);
-			// derived
-			var x = click.coords[0];
-			var y = click.coords[1];
+    var grabbedP = false;
 
-			if (grabbedP) {
-				if (pulse === 1)
-					slide(x, y);
+    // init slider position
+    m_trans.set_translation_obj_rel(knobObj, sliderLength * (defVal - 0.5), 0, 0.1, railObj);
 
-				if (pulse === -1) {
-					grabbedP = false;
-					return;
-				}
-			}
-			else if (!grabbedP) {
-				if (click.which === 1) {
-					obj = m_scenes.pick_object(x, y);
-					if (obj !== null && b4wObjEqual(obj, railObj)) {
-						grabbedP = true;
-						startTrans = m_trans.get_translation(obj);
-						startCanvasX = x;
-						startCanvasY = y;
-					}
-				}
-				else {
-					return;
-				}
-			}
+    function slide(x, y) {
 
-		};
+      function rayTestCb(id, hitFraction, objHit, hitTime, hitPos, hitNorm) {
+        // objHit is always calculated against the center of mass.
+        var normalizedOutput = hitPos[0] / sliderLength + 0.5;
+        console.log(normalizedOutput);
+        if (normalizedOutput > 0 && normalizedOutput < 1)
+          m_trans.set_translation_obj_rel(knobObj, hitPos[0], 0, 0.1, railObj);
+        if (railNodemat)
+          m_material.set_nodemat_value(railObj, railNodemat, normalizedOutput);
+        if (knobNodemat)
+          m_material.set_nodemat_value(knobObj, knobNodemat, normalizedOutput);
 
-		m_ctl.create_sensor_manifold(knobObj,
-																 "mouse",
-																 m_ctl.CT_CONTINUOUS,
-																 [mouseSensor, clickSensor],
-																 logic,
-																 cb
-																);
-	}
+      }
+
+      m_cam.calc_ray(camObj, x, y, pline);
+      m_math.get_pline_directional_vec(pline, to);
+      m_vec3.scale(to, 100, to);
+      var id = m_phy.append_ray_test_ext(
+        camObj,
+        from,
+        to,
+        "hud",
+        rayTestCb,
+        true,
+        false,
+        true,
+        true
+      );
+
+    }
+
+    function logic(triggers) {
+      if (triggers[1])
+        return 1;
+      else
+        return 0;
+    }
+
+    function cb(obj, id, pulse, param) {
+      var pxPerFrame = m_ctl.get_sensor_value(obj, id, 0);
+      var pos = m_ctl.get_sensor_payload(obj, id, 0);
+      var click = m_ctl.get_sensor_payload(obj, id, 1);
+      // derived
+      var x = click.coords[0];
+      var y = click.coords[1];
+
+      if (grabbedP) {
+        if (pulse === 1) {
+          slide(x, y);
+          return;
+        }
+        if (pulse === -1) {
+          m_app.enable_camera_controls();
+          grabbedP = false;
+          return;
+        }
+      }
+      else if (!grabbedP) {
+        if (click.which === 1) {
+          obj = m_scenes.pick_object(x, y);
+          if (obj !== null && b4wObjEqual(obj, railObj)) {
+            m_app.disable_camera_controls();
+            grabbedP = true;
+          }
+        }
+        else {
+          return;
+        }
+      }
+
+    };
+
+    m_ctl.create_sensor_manifold(knobObj,
+                                 "mouse",
+                                 m_ctl.CT_CONTINUOUS,
+                                 [mouseSensor, clickSensor],
+                                 logic,
+                                 cb
+                                );
+  }
 });
 
 // import the app module and start the app by calling the init method
